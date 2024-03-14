daa1db168eed51add5e3c061cdad70406f6e113d
Apachian <singhbhagatsingh91@gmail.com>
2024-03-14 14:14:48 +0530

    CRUD App

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    name: "Deepak Rathi",
+    email: "deepak@gmail.com",
+    id: 1,
+  },
+  {
+    name: "Sohail Kahna",
+    email: "sohail@gmail.com",
+    id: 2,
+  },
+  {
+    name: "Asmit Mishra",
+    email: "asmit@gmail.com",
+    id: 3,
+  },
+];
